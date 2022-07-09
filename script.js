05b0b7d1aa0196f2f1e00fa362856f37734d57dd
cassia <cassiastefani25@gmail.com>
2022-07-09 12:11:48 -0300

    melhorias imagens

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ const pular = () => {
         jorel.classList.remove('pulo');
     } ,500)
 }
+const roletaImg = () =>{
+    const gerador = Math.floor(Math.random()*4);
+    console.log(gerador);
+        if( gerador=== 1){
+            jorel.src = 'img/happy.jpg';
+            jorel.classList.add('src');
+            jorel.classList.remove('jorel');
+        } else if(gerador === 2){
+            jorel.src = 'img/sad.jpg';
+            jorel.classList.add('src');
+            jorel.classList.remove('jorel');
+        }else if(gerador=== 3){
+            jorel.src = 'img/curioso.jpg'
+            jorel.classList.add('src');
+            jorel.classList.remove('jorel');
+        }
+        else{
+            jorel.src= 'img/furioso.jpg';
+            jorel.classList.add('src');
+            jorel.classList.remove('jorel');
+        }
+                
+}
 const loop = setInterval(() =>{
     const position = abacate.offsetLeft;
     const posAbacate = +window.getComputedStyle(jorel).bottom.replace('px','');
@@ -26,22 +49,5 @@ const loop = setInterval(() =>{
         roletaImg();
     }
 }, 10)
-const roletaImg = () =>{
-    const gerador = Math.floor(Math.random()*4);
-        if( gerador=== 1){
-            jorel.src = 'img/happy.jpg';
-            jorel.style.width = '500px';
-            body.style.backgroundColor = '#7db26a';
-        } else if(gerador === 2){
-            jorel.src = 'img/sad.jpg';
-            jorel.style.width = '100%';
-            jorel.style.height = '100%';
-        } else{
-            jorel.src= 'img/furioso.jpg';
-            jorel.style.width = '100%';
-            jorel.style.height = '100%';
-        }
-                
-}
 
 document.addEventListener('keydown',pular);
